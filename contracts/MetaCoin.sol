eaa738be860537e556e8c396929b94c79724b0f7
sogoiii <angellopozo@gmail.com>
2018-01-16 12:39:46 -0800

    Updated contracts to use solidity 0.4.18

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
 
 import "./ConvertLib.sol";
 
@@ -13,35 +13,36 @@ contract MetaCoin {
 
 	modifier onlyAfterDate() {
 		if(now <= endTime) {
-			throw;
+			revert();
 		}
 		_;
 	}
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	function MetaCoin() {
+	function MetaCoin() public {
 		balances[tx.origin] = 10000;
 		endTime = now + 1 hours;
 	}
 
-	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
+	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+		if (balances[msg.sender] < amount)
+		 return false;
 		balances[msg.sender] -= amount;
 		balances[receiver] += amount;
 		Transfer(msg.sender, receiver, amount);
 		return true;
 	}
 
-	function getBalanceInEth(address addr) returns(uint){
+	function getBalanceInEth(address addr) public view returns(uint) {
 		return ConvertLib.convert(getBalance(addr),2);
 	}
 
-	function getBalance(address addr) returns(uint) {
+	function getBalance(address addr) public view returns(uint) {
 		return balances[addr];
 	}
 
-	function specialFn() onlyAfterDate returns(bool) {
+	function specialFn() onlyAfterDate public view returns(bool) {
 		return true;
 	}
 }
